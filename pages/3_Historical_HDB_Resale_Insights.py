367edbf1f737cec1d205f5690d7952cbb6e5a448
joeytxy <joeytan0706@live.com>
2024-10-24 14:05:03 +0800

    Modify Code

--- pages/3_Historical_HDB_Resale_Insights.py
+++ pages/3_Historical_HDB_Resale_Insights.py
@@ -38,7 +38,7 @@ with st.form("Input"):
 
 df = pd.read_csv("ResaleflatpricesbasedonregistrationdatefromJan2017onwards.csv")
 
-@st.cache_data(show_spinner=False)
+@st.cache_resource(show_spinner=False)
 def data_ai(topic):
     pandas_tool_agent = create_pandas_dataframe_agent(
         llm=ChatOpenAI(temperature=0, model='gpt-4o-mini'),
